daef7664baed96ec81aa4f95e947737aac9f821c
Mikhail Voloshin <126315733+Shalfei92@users.noreply.github.com>
2023-02-26 11:31:18 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,33 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("\t\t   *\t\t\t\t\t\t\t\t\t*\t\t\t\t\t\t\t\t\t*\n" + //1 строка
+                "\t\t  ***\t\t\t\t\t\t\t\t\t  *\t\t\t\t\t\t\t\t  *\n" +//2 строка
+                "\t\t *****\t\t\t*\t\t\t*******\t\t\t*\t\t\t\t\t\t\t*\n" +//3 строка
+                "\t\t*******\t\t\t**\t\t\t******\t\t\t  *\t\t\t\t\t\t  *\n" +//4строка
+                "\t   *********\t\t***\t\t\t*****\t\t\t\t*\t\t\t\t\t*\n" +//5строка
+                "\t  ***********\t\t****\t\t****\t\t\t\t  *\t\t\t\t  *\n" +//6строка
+                "\t *************\t\t*****\t\t***\t\t\t\t\t\t*\t\t\t*\n" +//7строка
+                "\t***************\t\t******\t\t**\t\t\t\t\t\t  *\t\t  *\n" +//8строка
+                "   *****************\t*******\t\t*\t\t\t\t\t\t\t*\t*\n\n\n");//9строка
+
+        System.out.println("\t\t\t*\t*\t\t\t\t\t\t *\t\t\t\t\t* * * * *\n" +
+                "\t\t*\t\t\t*\t\t\t\t\t***\n" +
+                "\t *\t\t\t\t   *\t\t\t   *****\t\t\t\t* * * * *\n" +
+                "\t*\t\t\t\t\t*\t\t\t  *******\n" +
+                "\t*\t\t\t\t\t*\t\t\t *********\t\t\t\t* * * * *\n" +
+                "\t  *\t\t\t\t  *\t\t\t\t***********\n" +
+                "\t\t*\t\t\t*\t\t\t   *************\t\t\t* * * * *\n" +
+                "\t\t\t*\t*\t\t\t\t  ***************\n" +
+                "\t\t\t\t\t\t\t\t *****************\t\t\t* * * * *\n" +
+                "\t\t\t\t\t\t\t\t  ***************\n" +
+                "\t\t\t\t\t\t\t\t   *************\t\t\t* * * * *\n" +
+                "\t\t\t\t\t\t\t\t\t***********\n" +
+                "\t\t\t\t\t\t\t\t\t *********\t\t\t\t* * * * *\n" +
+                "\t\t\t\t\t\t\t\t\t  *******\n" +
+                "\t\t\t\t\t\t\t\t\t   *****\t\t\t\t* * * * *\n" +
+                "\t\t\t\t\t\t\t\t\t    ***\n" +
+                "\t\t\t\t\t\t\t\t\t\t *\t\t\t\t\t* * * * *\n");
+
+
+    }
+}
